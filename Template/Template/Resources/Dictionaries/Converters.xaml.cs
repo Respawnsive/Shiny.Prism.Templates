fa6aa089ac80856af86c8cd90c92def11d7e5789
Jérémy BRUN-PICARD <jeremy.brunpicard@respawnsive.com>
2020-04-06 13:03:26 +0200

    Implementing merged dictionaries

---
+++ Template/Template/Resources/Dictionaries/Converters.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace Template.Resources.Dictionaries
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Converters
+    {
+        public Converters()
+        {
+            InitializeComponent();
+        }
+    }
+}
