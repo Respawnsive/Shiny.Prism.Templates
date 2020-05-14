49e3a624553ae428b7fc4a60dd41217c023afd3e
Jérémy BRUN-PICARD <jeremy.brunpicard@respawnsive.com>
2020-05-14 16:23:39 +0200

    Lighter LoggingModule

--- Templates/Template.Mobile/Modules/LoggingModule.cs
+++ Templates/Template.Mobile/Modules/LoggingModule.cs
@@ -1,11 +1,7 @@
 ﻿using System.Diagnostics;
-using System.Threading.Tasks;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Crashes;
 using Microsoft.Extensions.DependencyInjection;
 using Shiny;
 using Shiny.Logging;
-using Template.Mobile.Helpers;
 using Template.Mobile.Services.Settings.App;
 
 namespace Template.Mobile.Modules
@@ -23,19 +19,6 @@ namespace Template.Mobile.Modules
             {
                 var appSettingsService = services.BuildServiceProvider().Resolve<IAppSettingsService>(true);
                 services.UseAppCenterLogging(appSettingsService.AppCenterSecret, appSettingsService.AppCenterTrackCrashes, appSettingsService.AppCenterTrackEvents);
-
-                AppCenter.LogLevel = LogLevel.Verbose;
-                Crashes.ShouldProcessErrorReport = report => true;
-                Crashes.ShouldAwaitUserConfirmation = () => false;
-                Task.Run(async () =>
-                {
-                    if (await Crashes.HasCrashedInLastSessionAsync())
-                    {
-                        var rapport = await Crashes.GetLastSessionCrashReportAsync();
-                        var detail = "Date du crash initial : " + rapport.AppErrorTime.ToString("dd/MM/yyyy HH:mm:ss");
-                        Logger.Write(rapport.StackTrace)(("LastSessionCrash", detail));
-                    }
-                });
             }
             catch (System.Exception ex)
             {
