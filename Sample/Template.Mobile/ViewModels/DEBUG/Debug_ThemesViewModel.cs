6c58c8575eb963c25d36e46e5ca5a29a81c97587
Jerome Liger <jerome.liger@respawnsive.com>
2020-06-29 09:16:41 +0200

    Rollback lost files

---
+++ Sample/Template.Mobile/ViewModels/DEBUG/Debug_ThemesViewModel.cs
@@ -0,0 +1,124 @@
+﻿using Prism.Navigation;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Template.Mobile.Helpers;
+
+namespace Template.Mobile.ViewModels
+{
+    public class Debug_ThemesViewModel : ViewModelBase
+    {
+        public Debug_ThemesViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            //Create commands
+            //LoadCommand = ExecutionAwareCommand.FromTask(LoadDatas).OnIsExecutingChanged(OnIsExecutingChanged);
+
+            //Subscribe to Properties changes
+            this.WhenAnyValue(x => x.SelectedTheme).Subscribe(LoadTheme).DisposeWith(DestroyWith);
+        }
+
+        
+        #region Services
+
+
+
+        #endregion
+
+
+        #region Properties
+
+        [Reactive]
+        public string SelectedTheme { get; set; }
+
+        [Reactive]
+        public int TestValue { get; set; }
+
+        public ObservableCollection<string> Themes { get; set; }
+
+        #endregion
+
+
+        #region Commands
+
+        //public ICommand LoadCommand { get; }
+
+        #endregion
+
+
+        #region Methods
+
+        private void LoadDatas()
+        {
+            try
+            {
+                //Load data for this ViewModel (fake)
+                Themes = new ObservableCollection<string>(new List<string>()
+                {
+                    "Acrylic",
+                    "AcrylicBlur",
+                    "AcrylicDarkBlur",
+                    "Dark",
+                    "Light"
+                });
+                TestValue = 2;
+                SelectedTheme = Enum.GetName(typeof(AppTheme), ThemeHelper.GetCurrentTheme());
+
+            }
+            catch (Exception ex)
+            {
+                DialogsService?.Toast(this["Msg_RedToast_Error_Unknown"]);
+                Logger.Write(ex);
+            }
+            finally
+            {
+
+            }
+            
+        }
+
+        private void LoadTheme(string themename)
+        {
+            if (!String.IsNullOrWhiteSpace(themename))
+            {
+                switch (themename)
+                {
+                    case "Acrylic":
+                        ThemeHelper.ApplyTheme(AppTheme.Acrylic);
+                        break;
+                    case "AcrylicBlur":
+                        ThemeHelper.ApplyTheme(AppTheme.AcrylicBlur);
+                        break;
+                    case "AcrylicDarkBlur":
+                        ThemeHelper.ApplyTheme(AppTheme.AcrylicDarkBlur);
+                        break;
+                    case "Dark":
+                        ThemeHelper.ApplyTheme(AppTheme.Dark);
+                        break;
+                    case "Light":
+                        ThemeHelper.ApplyTheme(AppTheme.Light);
+                        break;
+                }
+            }
+        }
+
+        #endregion
+
+
+        #region LifeCycle
+
+        public override void Initialize(INavigationParameters parameters)
+        {
+            base.Initialize(parameters);
+            Task.Run(() => LoadDatas());
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ Sample/Template.Mobile/Views/DEBUG/Debug_ThemesView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Prism.Commands;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Template.Mobile.Helpers;
+using Template.Mobile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Template.Mobile.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Debug_ThemesView : ContentViewBase
+    {
+        public Debug_ThemesView() : base(nameof(Debug_ThemesView))
+        {
+            try
+            {
+                InitializeComponent();
+
+                // TODO -> NEVER do this in real code
+                // Simulating a complex view
+                Task.Delay(TimeSpan.FromSeconds(3)).Wait();
+
+                LaunchFakeProgress();
+            }
+            catch (Exception ex)
+            {
+                Logger.Write(ex);
+            }
+        }
+
+        private async void LaunchFakeProgress()
+        {
+            try
+            {
+                await pgbar.ProgressTo(1, 5000, Easing.Linear);
+                await pgbar.ProgressTo(0, 5000, Easing.Linear);
+                await pgbar.ProgressTo(1, 5000, Easing.Linear);
+                await pgbar.ProgressTo(0, 5000, Easing.Linear);
+                await pgbar.ProgressTo(1, 5000, Easing.Linear);
+                await pgbar.ProgressTo(0.5, 2500, Easing.Linear);
+            }
+            catch(Exception ex)
+            {
+                Logger.Write(ex);
+            }
+        }
+    }
+}
