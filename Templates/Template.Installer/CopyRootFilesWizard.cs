558299c968729028caf1beb8aa1a5b5b8c545296
Jerome Liger <jerome.liger@respawnsive.com>
2020-07-06 14:59:41 +0200

    files forgotten

---
+++ Templates/Template.Installer/CopyRootFilesWizard.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace Template.Installer
+{
+    public class CopyRootFilesWizard : IWizard
+    {
+        DTE _dte;
+
+        // This method is called before opening any item that
+        // has the OpenInEditor attribute.
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        // This method is only called for item templates,
+        // not for project templates.
+        public void ProjectItemFinishedGenerating(ProjectItem
+            projectItem)
+        {
+        }
+
+        // This method is called after the project is created.
+        public void RunFinished()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            try
+            {
+                Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+                _dte = automationObject as DTE;
+
+                var SolutionPath = replacementsDictionary["$solutiondirectory$"];
+                var assembly = Assembly.GetExecutingAssembly();
+                var resourceName = "MyCompany.MyProduct.MyFile.txt";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // This method is only called for item templates,
+        // not for project templates.
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
