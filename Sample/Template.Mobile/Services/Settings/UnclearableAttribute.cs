336ad8a072fce5d289ac1ce7c04aba496f0c0a71
Jérémy BRUN-PICARD <jeremy.brunpicard@respawnsive.com>
2020-07-22 13:29:01 +0200

    Including missing files

---
+++ Sample/Template.Mobile/Services/Settings/UnclearableAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Mobile.Services.Settings
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class UnclearableAttribute : Attribute
+    {
+    }
+}
