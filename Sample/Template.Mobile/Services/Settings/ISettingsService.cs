79ed54e5d899dd71cfa76523201e1b5a4009d2a7
Jérémy BRUN-PICARD <jeremy.brunpicard@respawnsive.com>
2020-07-22 13:09:18 +0200

    Optimizing settings clearing feature

--- Sample/Template.Mobile/Services/Settings/ISettingsService.cs
+++ Sample/Template.Mobile/Services/Settings/ISettingsService.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 
 namespace Template.Mobile.Services
 {
@@ -6,9 +7,17 @@ namespace Template.Mobile.Services
     {
         IAppSettingsService AppSettings { get; }
         CultureInfo SelectedCulture { get; }
+
+
+        #region Demonstration only - should be removed
+
         string UnclearableString { get; set; }
+        string ClearableString { get; set; }
+        string ClearableWithDefaultString { get; set; }
         bool ClearableBool { get; set; }
-        string SecureClearableString { get; set; }
+        string SecureClearableString { get; set; } 
+
+        #endregion
 
         void Clear();
     }
